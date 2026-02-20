8573bc5064b833259dc6bcd8dc23da19abe5bb77
Samuele <samuele.tonelli@pmpayment.com>
2026-02-20 10:59:05 +0100

    Initial commit.

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { StatusBar, useColorScheme } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { MainFlowCoordinator } from './src/MainFlowCoordinator';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <MainFlowCoordinator />
+    </SafeAreaProvider>
+  );
+}
+
+export default App;
---
+++ src/MainFlowCoordinator.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+import { commonStyles } from "./styles/styles"
+
+export const MainFlowCoordinator = () => {
+  return (
+    <View style={commonStyles.container}>
+      <Text>{"MainFlowCoordinator"}</Text>
+    </View>
+  )
+}
